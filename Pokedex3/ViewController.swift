8c65a50359aec0c463974378868974441f0c8e67
Ezgi AKÇA <ezgi.akca@ezgiakcamacpro.local>
2016-11-08 13:43:40 +0300

    Initial Commit

---
+++ Pokedex3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pokedex3
+//
+//  Created by Ezgi AKÇA on 08/11/2016.
+//  Copyright © 2016 Ezgi AKÇA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
